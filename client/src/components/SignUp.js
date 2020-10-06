feb6e3b8703401553d46caef2d4079521a6486dc
vishnuvardhan <vishnuvardhanbalasubramanian@gmail.com>
2020-10-06 16:32:08 +0530

    validation error

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,5 +1,11 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import isEmpty from 'validator/lib/isEmpty';
+import isEmail from 'validator/lib/isEmail';
+import equals from 'validator/lib/equals';
+import {showErrorMsg, showSuccessMsg} from '../helpers/messages';
+import {showLoading} from '../helpers/loading';
+import {signUp} from '../api/auth';
 import './SignUp.css';
 
 const SignUp = ()  => {
@@ -18,19 +24,68 @@ const SignUp = ()  => {
     const handleChange = e =>{
         setData({
             ...data,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
+            successMsg:'',
+            errorMsg:''
         })
     }
 
     const handleSubmit = e =>{
         e.preventDefault();
-        console.log(data);
+        //client side validation
+        if(isEmpty(userName) || isEmpty(email) || isEmpty(password) || isEmpty(password2)){
+            setData({
+                ...data,
+                errorMsg:'All Fields are required..'
+            })
+        } else if(!isEmail(email)){
+            setData({
+                ...data,
+                errorMsg:'Invalid Email'
+            })
+        } else if(!equals(password,password2)){
+            setData({
+                ...data,
+                errorMsg:'Passwords do not match'
+            })
+        } else {
+            // success
+            const {userName,email,password} = data;
+            const formData = {userName,email,password}
+            setData({
+                ...data,
+                loading:true
+            })
+
+            signUp(formData)
+            .then(response =>{
+                console.log(response);
+                setData({
+                    userName:'',
+                    email:'',
+                    password:'',
+                    password2:'',
+                    loading:false,
+                    successMsg: response.data.successMsg
+                })
+            })
+            .catch(err =>{
+                console.log('Axios signup Error:', err);
+                setData({
+                    ...data,
+                    loading:false
+                })
+            })
+        }
     }
-    
+
     return (
         <div className="img-container">
             <div className="row px-2 vh-100">
                 <div className="col-md-5 mx-auto align-self-center">
+                    {successMsg && showSuccessMsg(successMsg)} 
+                    {errorMsg && showErrorMsg(errorMsg)}
+                    {loading && (<div className="text-center pb-4">{showLoading()}</div>)}
                 <form className="signup-form mt-5" onSubmit={handleSubmit}>
                 <div className="form-group input-group">
                     <div className="input-group-prepend">
@@ -79,7 +134,9 @@ const SignUp = ()  => {
                </form>
                 </div>
             </div>
+            {JSON.stringify(data)}
         </div>
+       
     )
 }
 
