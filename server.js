69625b2e65113a94d8f2de00a11d21d2285c4e3a
vishnuvardhan <vishnuvardhanbalasubramanian@gmail.com>
2020-10-06 17:02:24 +0530

    concurrently setup

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 const connectDb = require('./database/db');
+
+//middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 connectDb();
 
